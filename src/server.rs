1b25d36cf4ea9578e67c0779ecfdf1b1a5913ccb
Camille Vauchel <camille.vauchel@neomi.immo>
2025-08-07 18:50:10 +0200

    Fix useless lifetime

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,39 @@
-use rocket::{get, routes};
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+
+use rocket::{post, routes};
 use rocket::{Build, Rocket};
 
 use crate::config::Config;
 
-#[get("/hello")]
-fn hello() -> &'static str {
+struct ApiKey;
+
+#[derive(Debug)]
+enum ApiKeyError {
+    Missing,
+    Invalid,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+    type Error = ApiKeyError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        /// Returns true if `key` is a valid API key string.
+        fn is_valid(key: &str) -> bool {
+            key == "valid_api_key"
+        }
+
+        match req.headers().get_one("authorization") {
+            None => Outcome::Error((Status::BadRequest, ApiKeyError::Missing)),
+            Some(key) if is_valid(key) => Outcome::Success(ApiKey),
+            Some(_) => Outcome::Error((Status::BadRequest, ApiKeyError::Invalid)),
+        }
+    }
+}
+
+#[post("/marius")]
+fn hello(_key: ApiKey) -> &'static str {
     "Hello, Rocket!"
 }
 
